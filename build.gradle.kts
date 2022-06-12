9d9ad6be242d8a0d07903bec8cae3196beca6d93
norhu1130 <norhu1130@naver.com>
2022-06-12 12:24:38 +0900

    Update Upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
 
 repositories {
+    maven("https://jitpack.io")
     mavenCentral()
     maven(paperMavenPublicUrl) {
         content { onlyForConfigurations(configurations.paperclip.name) }
@@ -17,7 +18,7 @@ repositories {
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.498.29")
-    paperclip("io.papermc:paperclip:3.0.2")
+    paperclip("com.github.KPaperMC:Paperclip:1825ad41c4")
 }
 
 allprojects {
